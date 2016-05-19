a107fb583b2c4792153a74b5431df9d87800436c
Qi <qizhao@Qis-MacBook-Pro.local>
2016-05-18 20:56:21 -0700

    main page created

---
+++ app/src/main/java/scu/book/campus/com/campusbook/ProfileSignup.java
@@ -0,0 +1,21 @@
+package scu.book.campus.com.campusbook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by qizhao on 5/18/16.
+ */
+public class ProfileSignup extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_signup);
+
+
+    }
+
+}
