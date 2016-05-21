dd9c5f9fa23c03df7cd89806218119c518b603b8
Qi <qizhao@Qis-MacBook-Pro.local>
2016-05-20 18:09:48 -0700

    sign up page changed

--- app/src/main/java/scu/book/campus/com/campusbook/ProfileSignup.java
+++ app/src/main/java/scu/book/campus/com/campusbook/ProfileSignup.java
@@ -1,5 +1,6 @@
 package scu.book.campus.com.campusbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,7 +15,14 @@ public class ProfileSignup extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_signup);
-
+        Button create = (Button) findViewById(R.id.button3);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileSignup.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
