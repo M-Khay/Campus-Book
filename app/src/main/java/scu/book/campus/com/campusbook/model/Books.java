49111380ac0e0cbd5f0bb966b3a166517475aa62
kushahuja <ahuja.kush.work@gmail.com>
2016-05-23 20:30:19 -0700

    models build

---
+++ app/src/main/java/scu/book/campus/com/campusbook/model/Books.java
@@ -0,0 +1,16 @@
+package scu.book.campus.com.campusbook.model;
+
+/**
+ * Created by kushahuja on 5/23/16.
+ */
+public class Books {
+    String bookName;
+    String bookPrice;
+    String bookImage;
+    String sellerEmail;
+    String sellerName;
+    String sellerLocation;
+    String isbn;
+
+    
+}
---
+++ app/src/main/java/scu/book/campus/com/campusbook/model/User.java
@@ -0,0 +1,43 @@
+package scu.book.campus.com.campusbook.model;
+
+
+/**
+ * Created by kushahuja on 5/23/16.
+ */
+public class User {
+    String name;
+    String email;
+    String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber= phoneNumber;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email= email;
+    }
+
+
+
+
+}
